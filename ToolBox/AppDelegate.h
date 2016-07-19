f3799249cadf29000ef88ae3c2d85fed9cd28be0
haiwei@cig <lihaiwei@cig.com.cn>
2016-07-19 14:13:42 +0800

    富文本常用封装

---
+++ ToolBox/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ToolBox
+//
+//  Created by wei on 16/7/19.
+//  Copyright © 2016年 vvlvv. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ToolBox/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ToolBox
+//
+//  Created by wei on 16/7/19.
+//  Copyright © 2016年 vvlvv. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
